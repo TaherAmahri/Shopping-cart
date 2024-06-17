f503a80e711b707e704d1debbfe1fbfce45b407d
Taher Amahri <141448060+TaherAmahri@users.noreply.github.com>
2024-06-17 19:28:01 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name": " LOUNGE CHAIR",
+        "price": 200,
+        "image": "images/1.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 2,
+        "name": " LOUNGE CHAIR",
+        "price": 250,
+        "image": "images/2.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 3,
+        "name": " LOUNGE CHAIR",
+        "price": 290,
+        "image": "images/3.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 4,
+        "name": " LOUNGE CHAIR",
+        "price": 200,
+        "image": "images/4.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 5,
+        "name": " LOUNGE CHAIR",
+        "price": 300,
+        "image": "images/5.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 6,
+        "name": " LOUNGE CHAIR",
+        "price": 200,
+        "image": "images/6.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 7,
+        "name": " LOUNGE CHAIR",
+        "price": 200,
+        "image": "images/7.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+    {
+        "id": 8,
+        "name": " LOUNGE CHAIR",
+        "price": 200,
+        "image": "images/8.png",
+        "description": "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus quos ipsum vero sunt, doloribus quis tenetur recusandae dolore repudiandae quasi minus magnam ipsa debitis impedit deserunt a provident voluptas."
+    },
+
+];
+export default products;
